0027a2b50b3751772a3f5ab08b0eed38e606cb47
boggogo <koemdjiev@gmail.com>
2017-02-08 22:32:06 +0000

    * Added new Activity * Added some of the necessary layout components for the DataCollectionActivity * Created necessary colours * Added String values

---
+++ app/src/main/java/georgikoemdzhiev/activeminutes/data_collection/view/DataCollectionActivity.java
@@ -0,0 +1,16 @@
+package georgikoemdzhiev.activeminutes.data_collection.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import georgikoemdzhiev.activeminutes.R;
+
+public class DataCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+
+    }
+}
--- app/src/main/java/georgikoemdzhiev/activeminutes/main_activity/view/ActiveMinutesActivity.java
+++ app/src/main/java/georgikoemdzhiev/activeminutes/main_activity/view/ActiveMinutesActivity.java
@@ -1,7 +1,9 @@
-package georgikoemdzhiev.activeminutes;
+package georgikoemdzhiev.activeminutes.main_activity.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import georgikoemdzhiev.activeminutes.R;
 
 public class ActiveMinutesActivity extends AppCompatActivity {
 
