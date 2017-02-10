b0f9aa2a9ad3cd2f47b34297d5b3f00e274188ec
boggogo <koemdjiev@gmail.com>
2017-02-10 19:18:43 +0000

    DataCollectionService --------------- * Added Sensor Manager and other necessary classes and variables to do the windowing of the raw sensor data * Implemented the Start & Stop Recording button's functionality

--- app/src/main/java/georgikoemdzhiev/activeminutes/services/DataCollectionService.java
+++ app/src/main/java/georgikoemdzhiev/activeminutes/services/DataCollectionService.java
@@ -2,17 +2,35 @@ package georgikoemdzhiev.activeminutes.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 
-public class DataCollectionService extends Service {
+public class DataCollectionService extends Service implements SensorEventListener {
+    // Class constants
     public static final String EXPORT_DATA = "export_data";
     public static final String STOP_RECORDING = "stop_recording";
     public static final String START_RECORDING = "start_recording";
     public static final String CLEAR_DATA = "clear_collected_data";
     public static final String CONTROL_KEY = "export_data_key";
+    // 3 Second time window
+    private static final long WINDOW_LENGTH = 3000;
 
+    private String activityLabel = "";
+    private SensorManager sensorManager;
+    private Sensor accSensor;
+    private long windowBegTime = -1;
 
     public DataCollectionService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     @Override
@@ -25,13 +43,14 @@ public class DataCollectionService extends Service {
                     break;
                 case START_RECORDING:
                     System.out.println("Recording...");
+                    sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_FASTEST);
                     break;
-
                 case CLEAR_DATA:
                     System.out.println("Data cleared!");
                     break;
                 case STOP_RECORDING:
-                    // TODO Maybe unregister sensor listener
+                    windowBegTime = -1;
+                    sensorManager.unregisterListener(this, accSensor);
                     System.out.println("Recording stopped!");
                     break;
 
@@ -41,13 +60,29 @@ public class DataCollectionService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;  // This Service is not a BindService
     }
 
 
     public void saveCollectedDataToDB() {
         //stopSelf();
         // TODO to be implemented
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (System.currentTimeMillis() - windowBegTime > WINDOW_LENGTH) {
+            if (windowBegTime > 0) {
+                // Feed data to HARManager here
+                System.out.println("Time Window Issued!");
+            }
+
+            windowBegTime = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
