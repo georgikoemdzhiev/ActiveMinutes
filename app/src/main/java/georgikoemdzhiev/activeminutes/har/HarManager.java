25ef3bc5e9b0c6bf1e79e7883f0b91fdb0a6973e
boggogo <koemdjiev@gmail.com>
2017-02-10 20:19:23 +0000

    * Added a stripped version of WEKA library to assist with the Machine Learning processes * Added the bases of the HarManager class

---
+++ app/src/main/java/georgikoemdzhiev/activeminutes/har/HarManager.java
@@ -0,0 +1,18 @@
+package georgikoemdzhiev.activeminutes.har;
+
+/**
+ * Created by koemdzhiev on 10/02/2017.
+ */
+
+public class HarManager implements IHarManager {
+
+    @Override
+    public void feedData(double[] xyz) {
+
+    }
+
+    @Override
+    public void issueTimeWindow() {
+
+    }
+}
---
+++ app/src/main/java/georgikoemdzhiev/activeminutes/har/IHarManager.java
@@ -0,0 +1,12 @@
+package georgikoemdzhiev.activeminutes.har;
+
+/**
+ * Created by koemdzhiev on 10/02/2017.
+ */
+
+public interface IHarManager {
+
+    void feedData(double[] xyz);
+
+    void issueTimeWindow();
+}
